dc93ccfa7362f42bb1a039c90d25dff076678f66
Joseph Kim <josephikim2@gmail.com>
2021-06-26 23:31:07 -0700

    validate LoginForm inputs on blur

--- src/client/components/LoginForm.js
+++ src/client/components/LoginForm.js
@@ -12,12 +12,13 @@ class LoginForm extends Component {
         password: '',
         passwordConfirm: ''
       },
-      validateUsernameOnChange: true,
-      validatePasswordOnChange: true,
-      validatePasswordConfirmOnChange: true,
+      validateUsernameOnChange: false,
+      validatePasswordOnChange: false,
+      validatePasswordConfirmOnChange: false,
       usernameError: '',
       passwordError: '',
-      passwordConfirmError: ''
+      passwordConfirmError: '',
+      submitCalled: false,
     }
   }
 
@@ -29,6 +30,80 @@ class LoginForm extends Component {
     // load locally stored returning user info
   }
   
+  handleBlur = event => {
+    const { target: { name, value } } = event;
+    switch (name) {
+      case 'username':
+        this.handleUsernameBlur(value);
+        break;
+      case 'password':
+        this.handlePasswordBlur(value);
+        break;
+      case 'passwordConfirm':
+        this.handlePasswordConfirmBlur(value);
+        break;
+      default:
+        return;
+    }
+  }
+
+  handleUsernameBlur = (value) => {
+    if (
+      this.state.validateUsernameOnChange === false &&
+      this.state.submitCalled === false
+    ) {
+      const newState = {
+        ...this.state,
+        formData: {
+          ...this.state.formData,
+          username: value
+        },
+        validateUsernameOnChange: true,
+        usernameError: validateFields.validateUsername(value)
+      }
+      this.setState(newState);
+    }
+    return;
+  }
+
+  handlePasswordBlur = (value) => {
+    if (
+      this.state.validatePasswordOnChange === false &&
+      this.state.submitCalled === false
+    ) {
+      const newState = {
+        ...this.state,
+        formData: {
+          ...this.state.formData,
+          password: value
+        },
+        validatePasswordOnChange: true,
+        passwordError: validateFields.validatePassword(value)
+      }
+      this.setState(newState);
+    }
+    return;
+  }
+
+  handlePasswordConfirmBlur = (value) => {
+    if (
+      this.state.validatePasswordConfirmOnChange === false &&
+      this.state.submitCalled === false
+    ) {
+      const newState = {
+        ...this.state,
+        formData: {
+          ...this.state.formData,
+          passwordConfirm: value
+        },
+        validatePasswordConfirmOnChange: true,
+        passwordConfirmError: validateFields.validatePasswordConfirm(value, this.state.formData.passsword)
+      }
+      this.setState(newState);
+    }
+    return;
+  }
+
   handleChange = (event) => {
     const { target: { name, value } } = event;
     const newState = {
@@ -67,8 +142,12 @@ class LoginForm extends Component {
               name='username'
               placeholder='Enter username' 
               onChange={this.handleChange}
+              onBlur={this.handleBlur}
               />
           </Form.Group>
+          <div className="text-danger">
+            <small>{this.state.usernameError}</small>
+          </div>
 
           <Form.Group controlId='password'>
             <Form.Label>Password</Form.Label>
@@ -77,8 +156,12 @@ class LoginForm extends Component {
               type='password' 
               placeholder='Enter password' 
               onChange={this.handleChange}
+              onBlur={this.handleBlur}
               />
           </Form.Group>
+          <div className="text-danger">
+            <small>{this.state.passwordError}</small>
+          </div>
 
           <Form.Group controlId='passwordConfirm'>
             <Form.Label>Confirm Password</Form.Label>
@@ -87,8 +170,12 @@ class LoginForm extends Component {
               type='password' 
               placeholder='Confirm password' 
               onChange={this.handleChange}
+              onBlur={this.handleBlur}
               />
           </Form.Group>
+          <div className="text-danger">
+            <small>{this.state.passwordConfirmError}</small>
+          </div>
 
           <Button 
             type='submit'

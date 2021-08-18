665f1166e5d861d0660f32026aebbb6fc9d73213
Joseph Kim <josephikim2@gmail.com>
2021-08-18 00:00:52 -0700

    redirect to login on requestToken null value

--- src/server/config/authConfig.js
+++ src/server/config/authConfig.js
@@ -1,10 +1,10 @@
 const SECRET = 'mysecretkey';
-const JWT_EXPIRATION = 3600;           // 1 hour
-const JWT_REFRESH_EXPIRATION = 86400;  // 24 hours
+// const JWT_EXPIRATION = 3600;           // 1 hour
+// const JWT_REFRESH_EXPIRATION = 86400;  // 24 hours
 
 /* for testing */
-// const JWT_EXPIRATION = 60;            // 1 minute
-// const JWT_REFRESH_EXPIRATION = 120;   // 2 minutes
+const JWT_EXPIRATION = 60;            // 1 minute
+const JWT_REFRESH_EXPIRATION = 120;   // 2 minutes
 
 const config = {
   SECRET,

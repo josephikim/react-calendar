dc2b5e008b770bebb43355be81f4e3916ec2045a
Joseph Kim <josephikim2@gmail.com>
2022-02-07 19:22:53 -0800

    fix baseURL value name

--- src/server/config/appConfigs.js
+++ src/server/config/appConfigs.js
@@ -1,5 +1,5 @@
 const calendarColors = ['2E86C1', '138D75', '28B463', 'D68910', 'BA4A00', 'E74C3C', '8E44AD'];
 
-const baseUrl = '/calendarapp';
+const baseURL = '/calendarapp';
 
-export { calendarColors, baseUrl };
+export { calendarColors, baseURL };

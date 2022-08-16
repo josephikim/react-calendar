752ab0174dd76d41c02280aefec8c0e541f0a57c
Joseph Kim <josephikim2@gmail.com>
2022-08-15 22:19:49 -0700

    add User model validation tests

--- src/server/models/User.unit.test.js
+++ src/server/models/User.unit.test.js
@@ -1,31 +1,72 @@
-import jest from 'Jest';
-
+import mongoose, { ValidationError } from 'mongoose';
 import db from 'server/models';
 
 const User = db.user;
-// const mongoose = db.mongoose;
 
-const mockRequest = (body) => ({
-  body
-});
-
-const mockResponse = () => {
-  const res = {};
-  res.status = jest.fn().mockReturnValue(res);
-  res.json = jest.fn().mockReturnValue(res);
-  return res;
-};
-
-describe('Registering user', () => {
-  it('should not validate user if username is empty string', async () => {
-    const invalidUser = { username: '', password: 'asdf' };
+describe('Validating user', () => {
+  it('Should return an error if username property is missing', async () => {
+    const invalidUser = { password: 'password' };
     const user = new User(invalidUser);
-    await expect(user.validate()).rejects.toThrow();
+    await expect(user.validate()).rejects.toThrowError(ValidationError);
   });
 
-  it('should not validate user if username is less than 4 characters', async () => {
-    const invalidUser = { username: 'joe', password: 'asdf' };
+  it('Should return an error if username is empty string', async () => {
+    const invalidUser = { username: '', password: 'password' };
     const user = new User(invalidUser);
-    await expect(user.validate()).rejects.toThrow();
+    await expect(user.validate()).rejects.toThrowError(ValidationError);
+  });
+
+  it('Should return an error if username is less than 4 characters', async () => {
+    const invalidUser = { username: 'joe', password: 'password' };
+    const user = new User(invalidUser);
+    await expect(user.validate()).rejects.toThrowError(ValidationError);
+  });
+
+  it('Should not return an error if username is more than 4 characters', async () => {
+    const invalidUser = { username: 'joey', password: 'password' };
+    const user = new User(invalidUser);
+    await expect(user.validate()).resolves.not.toThrowError(ValidationError);
+  });
+
+  it('Should return an error if password property is missing', async () => {
+    const invalidUser = { username: 'joey' };
+    const user = new User(invalidUser);
+    await expect(user.validate()).rejects.toThrowError(ValidationError);
+  });
+
+  it('Should return an error if password is empty string', async () => {
+    const invalidUser = { username: 'joey', password: '' };
+    const user = new User(invalidUser);
+    await expect(user.validate()).rejects.toThrowError(ValidationError);
+  });
+
+  it('Should return an error if password is less than 4 characters', async () => {
+    const invalidUser = { username: 'joey', password: 'pas' };
+    const user = new User(invalidUser);
+    await expect(user.validate()).rejects.toThrowError(ValidationError);
+  });
+
+  it('Should not return an error if password is more than 4 characters', async () => {
+    const invalidUser = { username: 'joey', password: 'password' };
+    const user = new User(invalidUser);
+    await expect(user.validate()).resolves.not.toThrowError(ValidationError);
+  });
+
+  it('Should return an error if roles is not an array', async () => {
+    const invalidUser = { username: 'joey', password: 'password', roles: {} };
+    const user = new User(invalidUser);
+    await expect(user.validate()).rejects.toThrowError(ValidationError);
+  });
+
+  it('Should return an error if roles is an array of type string', async () => {
+    const invalidUser = { username: 'joey', password: 'password', roles: ['role1', 'role2'] };
+    const user = new User(invalidUser);
+    await expect(user.validate()).rejects.toThrowError(ValidationError);
+  });
+
+  it('Should not return an error if roles is an array of type Mongoose ObjectId', async () => {
+    const invalidUser = { username: 'joey', password: 'password', roles: [new mongoose.Types.ObjectId()] };
+    const user = new User(invalidUser);
+    await expect(user.validate()).resolves.not.toThrowError(ValidationError);
   });
 });

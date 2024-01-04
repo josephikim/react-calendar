2abbf27d825ff40a6462a48daa2835733d6c1ee8
Joseph Kim <josephikim2@gmail.com>
2024-01-03 19:46:03 -0800

    use 2024 dates in seedHolidayEvents

--- seedHolidayEvents.js
+++ seedHolidayEvents.js
@@ -20,7 +20,7 @@ const getHolidays = () => {
       params: {
         api_key: API_KEY,
         country: 'US',
-        year: 2023,
+        year: 2024,
         type: 'national'
       }
     });
